f0027acca316e9907896b0bb1f3b289a6788c23a
Naveen Kumar Konam <52211514+naveenkonam1985@users.noreply.github.com>
2023-08-14 11:37:36 +0530

    Update streamlit_app.py
    Added fruit picker based on index

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,21 @@ import pandas
 
 streamlit.title("My Mom's New Healthy Diner")
 
+# First header
 streamlit.header('Breakfast Favorites')
+
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+
+# Second header
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
