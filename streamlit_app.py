d5f2d38c530875c95cefaab9fddec4ca94715da8
Naveen Kumar Konam <52211514+naveenkonam1985@users.noreply.github.com>
2023-08-15 13:15:19 +0530

    Update streamlit_app.py
    Added my_cnx.close() for closing connections at steamlit buton codes

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,6 +64,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 
@@ -78,4 +79,5 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfru
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
